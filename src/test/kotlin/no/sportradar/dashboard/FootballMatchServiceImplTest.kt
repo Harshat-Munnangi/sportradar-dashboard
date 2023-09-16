291f91063e9f9b54a7839901ea65169a80734aed
Harshat-Munnangi <munnangi.harshat@gmail.com>
2023-09-16 23:33:59 +0200

    repeat index starts at 0

--- src/test/kotlin/no/sportradar/dashboard/FootballMatchServiceImplTest.kt
+++ src/test/kotlin/no/sportradar/dashboard/FootballMatchServiceImplTest.kt
@@ -38,19 +38,15 @@ class FootballMatchServiceImplTest {
     @Test
     fun `should throw error if match already exists` () {
         repeat(3) { footballMatchServiceImpl.startMatch("Home$it", "Away$it") }
-        assertThrows<MatchAlreadyExistsException> {
-            footballMatchServiceImpl.startMatch("Home2", "Away2")
-        }
-        footballMatchServiceImpl.startMatch("Home4", "Away4")
+        assertThrows<MatchAlreadyExistsException> { footballMatchServiceImpl.startMatch("Home1", "Away1") }
+        footballMatchServiceImpl.startMatch("Home3", "Away3")
         assertThat(footballMatchServiceImpl.getSummaryDashboard().size).isEqualTo(4)
     }
 
     @Test
     fun `should throw error if any team is busy in other match` () {
         repeat(2) { footballMatchServiceImpl.startMatch("Home$it", "Away$it") }
-        assertThrows<TeamAlreadyBusyException> {
-            footballMatchServiceImpl.startMatch("Home3", "Away1")
-        }
+        assertThrows<TeamAlreadyBusyException> { footballMatchServiceImpl.startMatch("Home3", "Away1") }
         footballMatchServiceImpl.startMatch("Home4", "Away4")
         assertThat(footballMatchServiceImpl.getSummaryDashboard().size).isEqualTo(3)
     }
@@ -59,7 +55,7 @@ class FootballMatchServiceImplTest {
     fun `should update a match score` () {
         repeat(2) { footballMatchServiceImpl.startMatch("Home$it", "Away$it") }
         assertThat(footballMatchServiceImpl.getSummaryDashboard().size).isEqualTo(2)
-        val modifiedMatch = FootballMatch("Home2", 4, "Away2", 7)
+        val modifiedMatch = FootballMatch("Home1", 4, "Away1", 7)
         footballMatchServiceImpl.updateMatchDetails(modifiedMatch)
         val updatedMatchList = footballMatchServiceImpl.getSummaryDashboard()
         assertThat(updatedMatchList.size).isEqualTo(2)
@@ -83,11 +79,11 @@ class FootballMatchServiceImplTest {
     fun `should remove a match once it is finished` () {
         repeat(2) { footballMatchServiceImpl.startMatch("Home$it", "Away$it") }
         assertThat(footballMatchServiceImpl.getSummaryDashboard().size).isEqualTo(2)
-        footballMatchServiceImpl.finishMatch(FootballMatch(homeTeamName = "Home2", awayTeamName = "Away2"))
+        footballMatchServiceImpl.finishMatch(FootballMatch(homeTeamName = "Home1", awayTeamName = "Away1"))
         val newMatchList = footballMatchServiceImpl.getSummaryDashboard()
         assertThat(newMatchList.size).isEqualTo(1)
-        assertThat(newMatchList.first().homeTeamName).isEqualTo("Home1")
-        assertThat(newMatchList.first().awayTeamName).isEqualTo("Away1")
+        assertThat(newMatchList.first().homeTeamName).isEqualTo("Home0")
+        assertThat(newMatchList.first().awayTeamName).isEqualTo("Away0")
     }
 
     @Test
@@ -104,14 +100,14 @@ class FootballMatchServiceImplTest {
     fun `should sort depending on total score` () {
         repeat(6) { footballMatchServiceImpl.startMatch("Home$it", "Away$it") }
         val dashboardList = footballMatchServiceImpl.getSummaryDashboard()
-        assertThat(dashboardList.first().homeTeamName).isEqualTo("Home6")
-        assertThat(dashboardList.last().homeTeamName).isEqualTo("Home1")
-        val match1 = FootballMatch("Home1", 3, "Away1", 2) //5
-        val match2 = FootballMatch("Home2", 6, "Away2", 7) //13
-        val match3 = FootballMatch("Home3", 7, "Away3", 2) //9
-        val match4 = FootballMatch("Home4", 2, "Away4", 9) //11
-        val match5 = FootballMatch("Home5", 4, "Away5", 2) // 6
-        val match6 = FootballMatch("Home6", 1, "Away6", 2) // 3
+        assertThat(dashboardList.first().homeTeamName).isEqualTo("Home5")
+        assertThat(dashboardList.last().homeTeamName).isEqualTo("Home0")
+        val match1 = FootballMatch("Home0", 3, "Away0", 2) //5
+        val match2 = FootballMatch("Home1", 6, "Away1", 7) //13
+        val match3 = FootballMatch("Home2", 7, "Away2", 2) //9
+        val match4 = FootballMatch("Home3", 2, "Away3", 9) //11
+        val match5 = FootballMatch("Home4", 4, "Away4", 2) // 6
+        val match6 = FootballMatch("Home5", 1, "Away5", 2) // 3
         footballMatchServiceImpl.updateMatchDetails(match3)
         footballMatchServiceImpl.updateMatchDetails(match5)
         footballMatchServiceImpl.updateMatchDetails(match1)
@@ -119,33 +115,33 @@ class FootballMatchServiceImplTest {
         footballMatchServiceImpl.updateMatchDetails(match2)
         footballMatchServiceImpl.updateMatchDetails(match6)
         val newDashboardList = footballMatchServiceImpl.getSummaryDashboard()
-        assertThat(newDashboardList.first().homeTeamName).isEqualTo("Home2")
-        assertThat(newDashboardList.last().awayTeamName).isEqualTo("Away6")
+        assertThat(newDashboardList.first().homeTeamName).isEqualTo("Home1")
+        assertThat(newDashboardList.last().awayTeamName).isEqualTo("Away5")
     }
 
     @Test
     fun `should sort depending on match start time if all total scores are equal` () {
         repeat(6) { footballMatchServiceImpl.startMatch("Home$it", "Away$it") }
         val dashboardList = footballMatchServiceImpl.getSummaryDashboard()
-        assertThat(dashboardList.first().homeTeamName).isEqualTo("Home6")
-        assertThat(dashboardList.last().homeTeamName).isEqualTo("Home1")
+        assertThat(dashboardList.first().homeTeamName).isEqualTo("Home5")
+        assertThat(dashboardList.last().homeTeamName).isEqualTo("Home0")
     }
 
     @Test
     fun `should sort depending on score and thereafter match start time` () {
         repeat(6) { footballMatchServiceImpl.startMatch("Home$it", "Away$it") }
-        val match3 = FootballMatch("Home3", 3, "Away3", 2)
-        val match5 = FootballMatch("Home5", 2, "Away5", 3)
-        val match6 = FootballMatch("Home6", 1, "Away6", 3)
+        val match3 = FootballMatch("Home2", 3, "Away2", 2)
+        val match5 = FootballMatch("Home4", 2, "Away4", 3)
+        val match6 = FootballMatch("Home5", 1, "Away5", 3)
         footballMatchServiceImpl.updateMatchDetails(match3)
         footballMatchServiceImpl.updateMatchDetails(match5)
         footballMatchServiceImpl.updateMatchDetails(match6)
         val newDashboardList = footballMatchServiceImpl.getSummaryDashboard()
         assertThat(newDashboardList.size).isEqualTo(6)
-        assertThat(newDashboardList.first().homeTeamName).isEqualTo("Home5")
-        assertThat(newDashboardList[1].homeTeamName).isEqualTo("Home3")
-        assertThat(newDashboardList[2].homeTeamName).isEqualTo("Home6")
-        assertThat(newDashboardList[3].homeTeamName).isEqualTo("Home4")
-        assertThat(newDashboardList.last().homeTeamName).isEqualTo("Home1")
+        assertThat(newDashboardList.first().homeTeamName).isEqualTo("Home4")
+        assertThat(newDashboardList[1].homeTeamName).isEqualTo("Home2")
+        assertThat(newDashboardList[2].homeTeamName).isEqualTo("Home5")
+        assertThat(newDashboardList[3].homeTeamName).isEqualTo("Home3")
+        assertThat(newDashboardList.last().homeTeamName).isEqualTo("Home0")
     }
 }
