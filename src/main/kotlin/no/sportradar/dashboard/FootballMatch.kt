7aa9bd841f26f9a6a3c7c1f25b8130e21ec7ae8e
Harshat-Munnangi <munnangi.harshat@gmail.com>
2023-09-16 22:24:06 +0200

    add exception classes

--- src/main/kotlin/no/sportradar/dashboard/FootballMatch.kt
+++ src/main/kotlin/no/sportradar/dashboard/FootballMatch.kt
@@ -1,5 +1,6 @@
 package no.sportradar.dashboard
 
+import java.lang.IllegalArgumentException
 import java.time.LocalDateTime
 
 data class FootballMatch(
@@ -9,3 +10,9 @@ data class FootballMatch(
     val awayTeamPoints: Int = 0,
     val created: LocalDateTime = LocalDateTime.now()
 )
+
+internal class MatchAlreadyExistsException(message: String = "") : IllegalArgumentException(message)
+
+internal class TeamAlreadyBusyException(message: String = "") : IllegalArgumentException(message)
+
+internal class NoMatchFoundException(message: String = "") : IllegalArgumentException(message)
