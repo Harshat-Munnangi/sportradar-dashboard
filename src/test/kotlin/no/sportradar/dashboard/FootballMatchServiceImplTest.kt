76abf15630b4073fadd036a707f2b656406be6b2
Harshat-Munnangi <munnangi.harshat@gmail.com>
2023-09-16 22:34:12 +0200

    add start match test cases

--- src/test/kotlin/no/sportradar/dashboard/FootballMatchServiceImplTest.kt
+++ src/test/kotlin/no/sportradar/dashboard/FootballMatchServiceImplTest.kt
@@ -3,6 +3,7 @@ package no.sportradar.dashboard
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class FootballMatchServiceImplTest {
 
@@ -27,5 +28,39 @@ class FootballMatchServiceImplTest {
             assertThat(it.homeTeamPoints).isEqualTo(0)
             assertThat(it.awayTeamPoints).isEqualTo(0)
         }
+    }
+
+    @Test
+    fun `should throw error if team name is empty` () {
+        footballMatchServiceImpl.startMatch("HomeTeam1", "AwayTeam1")
+        footballMatchServiceImpl.startMatch("HomeTeam2", "AwayTeam2")
+        assertThrows<IllegalArgumentException> {
+            footballMatchServiceImpl.startMatch("HomeTeam3", "")
+        }
+        footballMatchServiceImpl.startMatch("HomeTeam4", "AwayTeam4")
+        assertThat(footballMatchServiceImpl.getSummaryDashboard().size).isEqualTo(3)
+    }
+
+    @Test
+    fun `should throw error if match already exists` () {
+        footballMatchServiceImpl.startMatch("HomeTeam1", "AwayTeam1")
+        footballMatchServiceImpl.startMatch("HomeTeam2", "AwayTeam2")
+        footballMatchServiceImpl.startMatch("HomeTeam3", "AwayTeam3")
+        assertThrows<MatchAlreadyExistsException> {
+            footballMatchServiceImpl.startMatch("HomeTeam2", "AwayTeam2")
+        }
+        footballMatchServiceImpl.startMatch("HomeTeam4", "AwayTeam4")
+        assertThat(footballMatchServiceImpl.getSummaryDashboard().size).isEqualTo(4)
+    }
+
+    @Test
+    fun `should throw error if any team is busy in other match` () {
+        footballMatchServiceImpl.startMatch("HomeTeam1", "AwayTeam1")
+        footballMatchServiceImpl.startMatch("HomeTeam2", "AwayTeam2")
+        assertThrows<TeamAlreadyBusyException> {
+            footballMatchServiceImpl.startMatch("HomeTeam3", "AwayTeam1")
+        }
+        footballMatchServiceImpl.startMatch("HomeTeam4", "AwayTeam4")
+        assertThat(footballMatchServiceImpl.getSummaryDashboard().size).isEqualTo(3)
     }
 }
